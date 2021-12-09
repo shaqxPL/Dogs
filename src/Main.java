fa7e1245a6a2a89cea4f85a92aefb3334af7099c
shaqxPL <tomasz198720@gmail.com>
2021-12-09 19:09:42 +0100

    Dogs

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main (String[] args) {
+       Dog pies1 = new Dog();
+        System.out.println(pies1);
+
+        Dog pies2 = new Dog();
+        System.out.println(pies2);
+
+        Dog pies3 = new Dog("azor", "kundel", 3);
+        System.out.println(pies3);
+
+        Dog pies4 = new Dog("pinczer");
+        System.out.println(pies4);
+
+    }
+}
