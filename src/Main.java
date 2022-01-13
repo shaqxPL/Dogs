8ff4b4ee640d13a192d6fce1c9d15032849c7445
shaqxPL <tomasz198720@gmail.com>
2022-01-13 18:54:16 +0100

    3Write a Java program to print the sum (addition), multiply, subtract, divide and remainder of two numbers.
    Test Data:
    Input first number: 125
    Input second number: 24
    Expected Output :
    125 + 24 = 149
    125 - 24 = 101
    125 x 24 = 3000
    125 / 24 = 5
    125 mod 24 = 5

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,29 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main (String[] args) {
+
         Scanner userInput = new Scanner(System.in);
+        // ln w tej samej linii
+        System.out.print("Input first number: ");
         int a = userInput.nextInt();
+        // ln linia ponizej
+        System.out.println("Input second number: ");
         int b = userInput.nextInt();
-        int sum = a + b;
-        System.out.println("wynik dodawania to " + sum);
+
+        System.out.println(a + " + " + b + " = " + (a+b));
+
+        int subtract = a - b;
+        System.out.println(a + " - " + b + " = " + subtract);
+
+        System.out.println(a + " x " + b + " = " + (a*b));
+
+        int divide = a / b;
+        System.out.println(a + " / " + b + " = " + divide);
+
+        int modulo = a % b;
+        System.out.println(a + " mod " + b + " = " + modulo);
 
 
 
